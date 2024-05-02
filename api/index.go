4b83d21ed13032ebf46904286643e7bf0fe4d7f9
TheWisePigeon <zozozozeph@gmail.com>
2024-05-02 22:04:47 +0000

    add dummy log

--- api/index.go
+++ api/index.go
@@ -1,10 +1,12 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Println("Received call")
   w.WriteHeader(http.StatusOK)
   return
 }
